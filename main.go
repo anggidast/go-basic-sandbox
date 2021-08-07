bb07f5443a74263940c8ccc268c8dd9c7661968c
anggidast <adastariana@gmail.com>
2021-08-07 23:35:28 +0700

    add switch statement at options prompt

--- main.go
+++ main.go
@@ -35,7 +35,25 @@ func promptOptions(b bill) {
 	reader := bufio.NewReader(os.Stdin)
 
 	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
-	fmt.Println(opt)
+
+	// * SWITCH STATEMENT
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		fmt.Println(name, price)
+	case "t":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+
+		fmt.Println(tip)
+	case "s":
+
+		fmt.Println("you choose s")
+	default:
+		fmt.Println("invalid option!")
+		promptOptions(b)
+	}
 }
 
 func main() {
