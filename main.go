7281bcde246f64d579f40dac06c2565cab418da9
anggidast <adastariana@gmail.com>
2021-08-07 23:15:00 +0700

    reusable prompt and input getter

--- main.go
+++ main.go
@@ -9,12 +9,21 @@ import (
 	"strings"
 )
 
+func getInput(prompt string, r *bufio.Reader) (string, error) { // * reusable to show prompt and get input
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
 func createBill() bill {
 	reader := bufio.NewReader(os.Stdin) // * NewReader method need the source of information, terminal = standard input (Stdin)
 
-	fmt.Print("Create a new bill name: ")
-	name, _ := reader.ReadString('\n') // * name is variable to receive user input after press enter, _ is actually an error
-	name = strings.TrimSpace(name)     // * remove any white space
+	// fmt.Print("Create a new bill name: ")
+	// name, _ := reader.ReadString('\n') // * name is variable to receive user input after press enter, _ is actually an error
+	// name = strings.TrimSpace(name)     // * remove any white space
+
+	name, _ := getInput("Create a new bill name: ", reader)
 
 	b := newBill(name)
 	fmt.Println("Created the bill - ", b.name)
@@ -22,8 +31,14 @@ func createBill() bill {
 	return b
 }
 
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+	fmt.Println(opt)
+}
+
 func main() {
 	myBill := createBill()
-
-	fmt.Println(myBill)
+	promptOptions(myBill)
 }
