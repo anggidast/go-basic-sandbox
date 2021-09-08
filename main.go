f7e5abb08e52df47440cfa8b1bbcf536192ff19c
anggidast <adastariana@gmail.com>
2021-09-08 22:14:25 +0700

    add others function

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func getInitials(n string) (string, string) { // * the func have 2 params with s
 
 	initials := []string{}
 	for _, v := range names {
-		initials = append(initials, v[:1]) // * push first character at every names elements (v), v[0] is cannot use at string var
+		initials = append(initials, v[:1]) // * push first character at every names elements (v), v[0] cannot use at string var
 	}
 
 	if len(initials) > 1 {
@@ -40,6 +40,49 @@ func getInitials(n string) (string, string) { // * the func have 2 params with s
 
 	return initials[0], "" // * return 1 value and 1 empty string value
 }
+
+// * Variadic Function
+// * is a function with dynamic parameters amount (works like spread operator/rest parameter in JS)
+func sumAll(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+
+	return total
+}
+
+// * Function as Value and as Parameter
+func getGoodBye(name string) string {
+	return "Good bye " + name
+}
+
+type Filter func(string) string // * bisa langsung bisa dengan type declaration
+
+func sayHelloWithFilter(name string, filter Filter) {
+	filteredName := filter(name)
+	fmt.Println("Hello", filteredName)
+}
+
+func spamFilter (name string) string {
+	if name == "Anjing" {
+		return "***"
+	} else {
+		return name
+	}
+}
+
+// * Anonymous Function
+type Blacklist func(string) bool
+func registerUser(name string, blacklist Blacklist) {
+	if blacklist(name) {
+		fmt.Println("You are blocked", name)
+	} else {
+		fmt.Println("Welcome", name)
+	}
+}
+
+// * Recursive Function
 
 // * func main is entry point for the application, only one in one file, will run first in file
 func main() {
@@ -58,13 +101,38 @@ func main() {
 	// 	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f \n", a1, a2)
 
 	// * Multiple Return Values
-	fn1, sn1 := getInitials("tifa lockhart") // * multi variabel store/declaration when it get multiple return
-	fmt.Println(fn1, sn1)
+	// fn1, sn1 := getInitials("tifa lockhart") // * multi variabel store/declaration when it get multiple return
+	// fmt.Println(fn1, sn1)
 
-	fn2, sn2 := getInitials("cloud strife")
-	fmt.Println(fn2, sn2)
+	// fn2, sn2 := getInitials("cloud strife")
+	// fmt.Println(fn2, sn2)
 
-	fn3, sn3 := getInitials("barret")
-	fmt.Println(fn3, sn3)
+	// fn3, sn3 := getInitials("barret")
+	// fmt.Println(fn3, sn3)
+
+	// * Variadic Function
+	// fmt.Println(sumAll(1,2,3,4,5))
+	
+	// * Send slice to variadic function
+	// slice := []int{1,2,3,4,5}
+	// fmt.Println(sumAll(slice...))
+
+	// * Function as value/variable
+	// goodBye := getGoodBye
+	// fmt.Println(goodBye("anggi"))
+
+	// * Function as parameter
+	// sayHelloWithFilter("Anggi", spamFilter)
+	// sayHelloWithFilter("Anjing", spamFilter)
+
+	// * Anonymous Function
+	blacklist := func(name string) bool {
+		return name == "admin"
+	}
+
+	registerUser("admin", blacklist)
+	registerUser("admin", func(name string) bool {
+		return name == "root"
+	})
 
 }
